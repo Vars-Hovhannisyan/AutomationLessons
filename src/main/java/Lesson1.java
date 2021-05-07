0b91fe1d99de65d227133920079f7abd27f9cee0
varshovhannisyan <varsenik.hovhannisyan@picsart.com>
2021-05-07 20:10:32 +0400

    Commit

--- src/main/java/Lesson1.java
+++ src/main/java/Lesson1.java
@@ -1,15 +1,12 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
 
 public class Lesson1 {
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args){
 
         System.setProperty("webdriver.chrome.driver", "src/chromedriver");
         ChromeDriver driver = new ChromeDriver();
